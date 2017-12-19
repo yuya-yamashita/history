5f8ad609a84415fe940d3dada4406b4c8f6f600a
山下侑哉 <yamashitayuuya@yamashitayuuya.local>
2017-12-19 22:01:31 +0900

    model uniqueness add

--- app/models/diary_tag.rb
+++ app/models/diary_tag.rb
@@ -1,6 +1,7 @@
 class DiaryTag < ApplicationRecord
   validates :diary_id, presence: true
   validates :tag_id, presence: true
+  validates :diary_id, :uniqueness => {:scope => :tag_id}
 
   belongs_to :diary
 	belongs_to :tag
--- app/models/user_tag.rb
+++ app/models/user_tag.rb
@@ -2,6 +2,7 @@ class UserTag < ApplicationRecord
   validates :user_id, presence: true
   validates :tag_id, presence: true
   validates :count, presence: true
+  validates :user_id, :uniqueness => {:scope => :tag_id}
 
   belongs_to :user
   belongs_to :tag
